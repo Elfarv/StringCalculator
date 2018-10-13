99684d9a2dedbf9e2ef50738a237dda81d0e4032
Elfarv <elfarv@gmail.com>
2018-10-13 20:42:16 +0000

    hægt að leggja saman 2 tolustafi

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ function add(numbers){
 	if(numbers.length == 1) { 
 		return parseInt(numbers); 
 	}
-
+	if(numbers.includes(",")){
+		var numberArrays = numbers.split(",");
+		return parseInt(numberArrays[0]) + parseInt(numberArrays[1]);
+	} 
+	else{
+		return parseInt(numbers);
+	}
 }
 
 module.exports = add;
--- index.test.js
+++ index.test.js
@@ -4,6 +4,10 @@ test(`should return zero on an empty string.`, () => {
     expect(add(``)).toBe(0);
 });
 
-test(`should return zero on an empty string.`, () => {
+test(`should return 1 on an string with 1.`, () => {
     expect(add(`1`)).toBe(1);
+});
+
+test(`should return 3 on an empty string  with 2 numbers.`, () => {
+  expect(add(`1,2`)).toBe(3);
 });
